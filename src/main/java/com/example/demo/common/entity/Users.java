3c678ffa48c180cc9636efc1223d9a787a0c0b1a
M-034 <yanagawa@insightech.co.jp>
2025-06-10 04:08:49 +0900

    Users.java実装

---
+++ src/main/java/com/example/demo/common/entity/Users.java
@@ -0,0 +1,12 @@
+package com.example.demo.common.entity;
+
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+public class Users {
+    private Integer id;
+    private String name;
+    private String department;
+    private LocalDate createdAt;
+}
