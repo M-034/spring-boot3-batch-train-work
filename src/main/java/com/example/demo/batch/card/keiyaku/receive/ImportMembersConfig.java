ea0f5ae0e154bab912dc17228c34a91823963d5f
M-034 <yanagawa0@gmail.com>
2025-07-12 15:52:58 +0900

    ImportMembersConfig実装

---
+++ src/main/java/com/example/demo/batch/card/keiyaku/receive/ImportMembersConfig.java
@@ -0,0 +1,115 @@
+package com.example.demo.batch.sample.master.user.receive;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+import com.example.demo.common.entity.Members;
+import com.example.demo.common.mapper.MembersMapper;
+import com.example.demo.core.exception.CustomSkipPolicy;
+import com.example.demo.core.listener.LogChunkListener;
+import com.example.demo.core.listener.LogJobListener;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ImportMembersConfig {
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final LogChunkListener logChunkListener;
+    private final LogJobListener logJobListener;
+    private final ImportMembersProcessor memberImportProcessor;
+    private final ImportMembersWriter memberImportWriter;
+    private final MembersMapper membersMapper;
+    
+    @Bean
+    public FlatFileItemReader<ImportMembersItem> reader() {
+        
+        FlatFileItemReader<ImportMembersItem> reader = new FlatFileItemReader<>();
+
+        // ヘッダー行をスキップ
+        reader.setLinesToSkip(1);
+        reader.setResource(new FileSystemResource("input-data/member.csv"));
+        reader.setLineMapper(new DefaultLineMapper<ImportMembersItem>() {
+            {
+                setLineTokenizer(new DelimitedLineTokenizer() {
+                    {
+                        setNames("keiyakuId", "memberId", "merchId", "cardNum", "createdAt");
+                    }
+                });
+                setFieldSetMapper(new BeanWrapperFieldSetMapper<ImportMembersItem>() {
+                    {
+                        setTargetType(ImportMembersItem.class);
+                    }
+                });
+            }
+        });
+        return reader;
+    };
+
+    @Bean
+    public Tasklet truncateTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
+                // membersテーブルを全件削除する
+                membersMapper.truncate();
+                // 処理が完了したことを示す
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    /**
+     * ジョブ
+     */
+    @Bean
+    public Job importMembersJob() {
+        return new JobBuilder("importMembersJob", jobRepository)
+                .start(importMembersStep1())
+                .next(importMembersStep2())
+                .listener(logJobListener) // ログ出力（ジョブ単位）
+                .build();
+    }
+
+    /**
+     * ステップ１
+     */
+    @Bean
+    public Step importMembersStep1() {
+        return new StepBuilder("importMembersStep1", jobRepository)
+                .tasklet(truncateTasklet(), platformTransactionManager)
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+    /**
+     * ステップ２
+     */
+    @Bean
+    public Step importMembersStep2() {
+        return new StepBuilder("importMembersStep2", jobRepository)
+                .<ImportMembersItem, Members>chunk(10, platformTransactionManager)
+                .reader(reader())
+                .processor(memberImportProcessor)
+                .writer(memberImportWriter)
+                .allowStartIfComplete(true) // true:何度でも再実行可能。false:一度だけ実行可能。
+                .faultTolerant()  // 耐障害性設定。例外等が発生しても処理を継続する。
+                .skipPolicy(new CustomSkipPolicy()) // カスタムスキップポリシーを設定。特定例外はスキップする。
+                .listener(logChunkListener) // ログ出力（チャンク単位）
+                .build();
+    }
+}
