92e906be59f0a96c4c483e74ff203dba3b069c32
M-034 <yanagawa0@gmail.com>
2025-07-12 16:47:52 +0900

    ImportMembersProcessor実装

---
+++ src/main/java/com/example/demo/batch/card/keiyaku/receive/ImportMembersProcessor.java
@@ -0,0 +1,20 @@
+package com.example.demo.batch.card.keiyaku.receive;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import com.example.demo.common.entity.Members;
+import com.example.demo.core.exception.SkipException;
+
+@Component
+public class ImportMembersProcessor implements ItemProcessor<ImportMembersItem, Members> {
+    @Override
+    public Members process(@NonNull ImportMembersItem item) throws Exception {
+        //商品IDが9999の時は登録しない
+        if ("9999".equals(item.getMerchId())) {
+            throw new SkipException("この顧客は登録できません");
+        }
+        //メンバーエンティティを生成して返却
+        return item.toMembers();
+    }
+}
